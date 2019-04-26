97411bb77867a8296d53fe2852bfdc3e9ad7e995
FKRauber <fabienne.rauber@gmail.com>
2019-04-26 12:13:35 -0400

    updated sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,36 @@ class SessionsController < ApplicationController
     @user = User.new
     @users = User.all
   end
-
   def create
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to signin_path, :notice => "Unable to sign you in, credentials unrecognized. Please try again."
+    end
   end
+
 
   def omniauth
+    if auth_hash = request.env["omniauth.auth"]
+      user = User.find_or_create_by_oa(auth_hash)
+      session[:user_id] = user.id
+      redirect_to root_path, :notice => "Signed In!"
+    else
+      redirect_to root_path, :notice => "Unable to sign you in, please check your credentials and try again."
+    end
   end
 
+
   def destroy
+    session.clear
+    redirect_to root_path, :notice => "Logged Out"
   end
 
 private
 
   def auth
+    request.env["omniauth.auth"]
   end
 end
