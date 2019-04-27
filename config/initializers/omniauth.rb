010b2d1663dbdd65f2e6fe41278cbd22dc3384a1
FKRauber <fabienne.rauber@gmail.com>
2019-04-27 11:46:19 -0400

    populated signin/up views

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
-puts "OMNIAUTH IS GO"
 
 Rails.application.config.middleware.use Omniauth::Builder do
     provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user"
 end
+
+puts "OMNIAUTH IS GO"
